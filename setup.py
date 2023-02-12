a9d7e67ab09358bcb4ec62dbcdc6d57d39b4f6a2
Shever Natsai <sobitha.natsai@adp.com>
2023-02-12 16:02:07 +0530

    enable pyproject.toml

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
-from policyserver import config
+# from policyserver import config
 #from dotenv import load_dotenv
 
 #load_dotenv()
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = config.VERSION
+__version__ = "0.0.1"
 
 # Get the long description from the README file
 with open(path.join(here, 'Readme.md'), encoding='utf-8') as f:
@@ -18,25 +18,14 @@ packages = [
     'policyserver.commands',
     'policyserver.core'
 ]
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '')
-                    for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='policyserver',
     version=__version__,
     packages=packages,
     include_package_data=True,
-    author='Shever',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
-    author_email='shevernts@gmail.com',
-    entry_points={
-        'console_scripts': [
-            'policyserver = policyserver.cli:main',
-        ]}
+    author='Shever Natsai',
+    # install_requires=install_requires,
+    # dependency_links=dependency_links,
+    author_email='shevernts@gmail.com'
 )
